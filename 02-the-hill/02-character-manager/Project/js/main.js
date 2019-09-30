a3fb4aba44eb56f7d7f7425b1534d50f477739b9
Yousef Alsatouf <yousef.alsatouf94@gmail.com>
2019-09-30 14:37:11 +0200

    added axios requiest

--- 02-the-hill/02-character-manager/Project/js/main.js
+++ 02-the-hill/02-character-manager/Project/js/main.js
@@ -1,1 +1,15 @@
-console.log("hello there from typ");
+(() => {
+    const axios = require('axios');
+
+    console.log(axios);
+
+
+
+    function greeter(person) {
+        return "Hello, " + person;
+    }
+    
+    let user = "Jane User";
+    
+    document.querySelector(".title").textContent = greeter(user);
+})();
